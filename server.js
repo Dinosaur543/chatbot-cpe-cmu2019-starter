0d86f72f0a9c0e80b72aaa11ea4b0258520d2cbe
Dinosaur <sou.spb@gmail.com>
2019-02-02 14:40:33 +0700

    add code

--- server.js
+++ server.js
@@ -14,19 +14,55 @@ app.get('/', function (req, res) {
     res.send('Hello World!!')
 })
 
-app.post('/webhook', middleware(config), (req, res) => {
+app.post('/webhook', middleware(config), (req, res) => {  //req = request
   const event = req.body.events[0];
   
   if (event.type === 'message') {
     const message = event.message;
     console.log(message)
-    client.replyMessage(event.replyToken, {
+    /* client.replyMessage(event.replyToken, {
       type: 'text',
-      text: message.type,//change
-    }
-    
-    
-    );
+      text: message.type,//change 
+      
+
+    }); */
+
+    client.replyMessage(event.replyToken, 
+      {
+        "type": "template",
+        "altText": "This is a buttons template",
+        "template": {
+            "type": "buttons",
+            "thumbnailImageUrl": "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png/revision/latest?cb=20150808131630",
+            "imageAspectRatio": "rectangle",
+            "imageSize": "cover",
+            "imageBackgroundColor": "#FFFFFF",
+            "title": "Menu",
+            "text": "Please select",
+            "defaultAction": {
+                "type": "uri",
+                "label": "View detail",
+                "uri": "http://google.com/"
+            },
+            "actions": [
+                {
+                  "type": "postback",
+                  "label": "Buy",
+                  "data": "action=buy&itemid=123"
+                },
+                {
+                  "type": "message",
+                  "label": "Add to cart",
+                  "text": "no no no"
+                },
+                {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://google.com"
+                }
+            ]
+        }
+      })
   }
   res.send('webhook success')//
   
@@ -38,3 +74,17 @@ app.set('port', (process.env.PORT || 4000))
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
